4c49fef218f7364b3e8fbe27d0d9c8df46eb4280
234959 <234959@KB37-309-C04>
2026-01-12 13:20:23 +0300

    MainPage сделано

---
+++ Para/Pages/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Para.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((sender as ListBox).SelectedItem is ListBoxItem choice)
+            {
+                string selectedEngine = choice.Content.ToString();
+                tb.Content = "Вы выбрали: " + selectedEngine;
+
+
+                ButtonNext.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
